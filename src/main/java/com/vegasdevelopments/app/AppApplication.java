199f45d96f43cdc6f1e03a12b5d25882476c178f
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2020-06-26 11:19:49 -0300

    added heroku

--- src/main/java/com/vegasdevelopments/app/AppApplication.java
+++ src/main/java/com/vegasdevelopments/app/AppApplication.java
@@ -1,17 +1,13 @@
 package com.vegasdevelopments.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
 }
--- src/main/java/com/vegasdevelopments/app/controllers/MainController.java
+++ src/main/java/com/vegasdevelopments/app/controllers/MainController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @RestController
 @CrossOrigin
 public class MainController {
-    @Autowired(required = false)
+    @Autowired
     AppRepository repository;
 
     @GetMapping
