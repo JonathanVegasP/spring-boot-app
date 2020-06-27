5425bbbf8acca64650eefad101eac11740eb4408
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2020-06-27 19:50:45 -0300

    fix gradle dependencies

--- src/main/java/com/vegasdevelopments/app/repositories/AesRepository.java
+++ src/main/java/com/vegasdevelopments/app/repositories/AesRepository.java
@@ -26,6 +26,8 @@ public class AesRepository {
         final SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
         final Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+
         return new HexBinaryAdapter().marshal(cipher.doFinal(input.getBytes(StandardCharsets.UTF_8)));
     }
 
